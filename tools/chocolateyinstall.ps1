c52e8f7be18c88402f865cb53480817a1fbf3beb
Oli Japa <olialexander1131@gmail.com>
2022-09-19 08:08:58 -0400

    SO package files created and configurated

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'temp.exe'
+
+$packagename = 'temp'
+
+$packageArgs = @{
+  packageName   = $packagename
+  #unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  file         = $fileLocation
+
+  softwareName  = 'temp*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+
+  validExitCodes= @(0)
+  silentArgs   = '/S'           # NSIS
+
+}
+
+Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
